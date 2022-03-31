75fb736dd4511ba91dffe7d4fd4f012427c08df7
JosephCorwin <corwin.joe@gmail.com>
2022-03-31 03:40:33 -0500

    cab factory
    also updated shops factory

--- spec/factories/shops.rb
+++ spec/factories/shops.rb
@@ -8,5 +8,13 @@ FactoryBot.define do
     address { "#{Faker::Address.full_address}" }
     phone { "#{Faker::PhoneNumber.phone_number}" }
     domain { "#{Faker::Lorem.word}.#{Faker::Lorem.word}" }
+
+    transient do
+      cab_count { 5 }
+    end
+
+    after(:create) do |shop, evaluator|
+      create_list(:cabs, evaluator.cab_count)
+    end
   end
 end
